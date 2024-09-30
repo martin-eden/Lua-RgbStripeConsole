315f8e300a00e023cb90fa10973a5b1902097ffd
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-09-30 20:01:40 +0200

    [~] [MakePlasm] Sync with [PlasmGenerator]..
    This snippet no longer contains plasm implementation.
    
    Kinda sad, I had fun writing it. Now implementation is scattered in
    [PlasmGenerator].
    
    But now logic is almost clear and all tuneable parameters are tuned.

--- MakePlasm.lua
+++ MakePlasm.lua
@@ -5,6 +5,11 @@
   structured infrastructure.
 
   2024-09-18
+
+  Rewritten using objectified plasm generator.
+  Returned to dull writing of nicely structured infrastructure.
+
+  2024-09-30
 ]]
 
 package.path = package.path .. ';../../?.lua'
@@ -12,202 +17,67 @@ require('workshop.base')
 
 local Stripe = request('StripeWriter.Interface')
 local Output = request('!.concepts.StreamIo.Output.File')
+local Plasm = request('PlasmGenerator.Interface')
 
+-- Stripe length in pixels
+local StripeLength = 60
+
+--[[
+  Brightness [0.0, 1.0]
+
+  I'm having brownout on my Arduino connected to keyboard (lol)
+  at full brightness.
+
+  Also for dark time, high brightness looks ugly.
+]]
+local Brightness = 0.27
+
+-- Name of file where we will write commands
 local OutputFileName = 'Data.Stripe'
+
+--
+
 Output:OpenFile(OutputFileName)
 
 Stripe:Init(Output)
 
 Stripe:Reset()
 
--- Stripe length in pixels
-local StripeLength = 60
+do
+  Plasm.MaxColorComponentValue = math.floor(Brightness * 0xFF)
 
---[[
-  Max color component value
-
-  I'm having USB interface brownout on my Arduino connected to
-  keyboard (lol) when most of pixel color components are 255.
-]]
-local MaxColorValue = 80
-
--- Just return valid random color
-local GetRandomColor =
-  function()
-    local Red = math.random(0, MaxColorValue)
-    local Green = math.random(0, MaxColorValue)
-    local Blue = math.random(0, MaxColorValue)
-    return { Red = Red, Green = Green, Blue = Blue }
-  end
-
-local LeftPixel =
-  {
-    Index = 0,
-    Color = GetRandomColor(),
-  }
-
-local RightPixel =
-  {
-    Index = StripeLength - 1,
-    Color = GetRandomColor(),
-  }
-
--- Roberto, can we have [-1, 1] random()?
-local SymmetricRandom =
-  function()
-    return (math.random() * 2.0) - 1.0
-  end
-
--- Static noise
-local StaticNoise =
-  function()
-    return SymmetricRandom() * 0.05
-  end
-
--- Linear noise
-local LinearNoise =
-  function(Distance)
-    return Distance
-  end
-
--- Quadratic noise
-local QuadraticNoise =
-  function(Distance)
-    return Distance ^ 2
-  end
-
--- Square root noise
-local SqrtNoise =
-  function(Distance)
-    return Distance ^ 0.5
-  end
-
--- Logarithmic noise
-local LogNoise =
-  function(Distance)
-    return math.log(Distance, 2)
-  end
-
--- Limit float value to given range
-local Clamp =
-  function(Value, MinValue, MaxValue)
-    if (Value < MinValue) then
-      return MinValue
-    end
-    if (Value > MaxValue) then
-      return MaxValue
-    end
-    return Value
-  end
-
--- Return some float in range [-1.0, +1.0]
-local MakeNoise =
-  function(Distance)
-    local DistanceNoiseFunc = LogNoise
-
-    local DistanceNoise = DistanceNoiseFunc(Distance)
-    local MaxDistanceNoise = DistanceNoiseFunc(StripeLength)
-
-    local Scale = 2.0
-
-    local Noise =
-      Scale *
-      (DistanceNoise / MaxDistanceNoise) *
-      SymmetricRandom() +
-      StaticNoise()
-
-    Noise = Clamp(Noise, -1.0, 1.0)
-
-    return Noise
-  end
-
--- Calculate middle integer between two integers
-local GetMiddle =
-  function(Left, Right)
-    return (Left + Right) // 2
-  end
-
--- Clamp value for color range [0, MaxColorValue]
-local ClampColorComponent =
-  function(Value)
-    if (Value < 0) then
-      return 0
-    end
-    if (Value > MaxColorValue) then
-      return MaxColorValue
-    end
-    Value = math.floor(Value)
-
-    return Value
-  end
-
--- Mix color component for two pixels at some distance
-local Mix =
-  function(ColorA, ColorB, Noise)
-    local Result
-
-    -- Actually we're summing two color signals here:
-    Result = GetMiddle(ColorA, ColorB) + (Noise * MaxColorValue)
-
-    Result = ClampColorComponent(Result)
-
-    return Result
-  end
-
-local GeneratePlasm
---[[
-  Recursive function to generate "plasm":
-  midway linear interpolation between pixels
-  with some distance-dependent noise.
-
-  It looks great for 2D, but I want to tinker with 1D version.
-]]
-GeneratePlasm =
-  function(LeftPixel, RightPixel)
-    local Distance = (RightPixel.Index - LeftPixel.Index) - 1
-
-    if (Distance <= 0) then
-      return
+  Plasm.SetPixel =
+    function(Pixel)
+      Stripe:SetPixel(Pixel)
+      -- Stripe:Display()
     end
 
-    -- print(string.format('Distance(%d) Noise(%.2f)', Distance, MakeNoise(Distance)))
+  Plasm.Scale = 6.0
 
-    local MidwayPixel =
-      {
-        Index = GetMiddle(LeftPixel.Index, RightPixel.Index),
-        Color =
-          {
-            Red =
-              Mix(
-                LeftPixel.Color.Red,
-                RightPixel.Color.Red,
-                MakeNoise(Distance)
-              ),
-            Green =
-              Mix(
-                LeftPixel.Color.Green,
-                RightPixel.Color.Green,
-                MakeNoise(Distance)
-              ),
-            Blue =
-              Mix(
-                LeftPixel.Color.Blue,
-                RightPixel.Color.Blue,
-                MakeNoise(Distance)
-              ),
-          },
-      }
+  Plasm.GetNoiseAmp =
+    function(Distance)
+      local Result
+      --[[ Circle arc
+      Result = (1 - (1 - Distance) ^ 2) ^ 0.5
+      -- Circle arc ]]
+      --[[ Full sine
+      local Angle_Deg = Distance * 180 - 90
+      local Angle_Rad = math.rad(Angle_Deg)
+      Result = (math.sin(Angle_Rad) + 1) / 2
+      -- Full sine ]]
+      --[[ Upper half sine
+      local Angle_Deg = Distance * 90
+      local Angle_Rad = math.rad(Angle_Deg)
+      Result = math.sin(Angle_Rad)
+      -- Upper half sine ]]
+      -- [[ Power of
+      Result = Distance ^ 0.5
+      -- Power of ]]
+      return Result
+    end
 
-    Stripe:SetPixel(MidwayPixel)
-    Stripe:Display()
-
-    GeneratePlasm(LeftPixel, MidwayPixel)
-    GeneratePlasm(MidwayPixel, RightPixel)
-  end
-
-Stripe:SetPixel(LeftPixel)
-Stripe:SetPixel(RightPixel)
-GeneratePlasm(LeftPixel, RightPixel)
+  Plasm:Generate(0, StripeLength - 1)
+end
 
 Stripe:Display()
 
@@ -216,4 +86,5 @@ Output:CloseFile()
 --[[
   2024-09-18
   2024-09-25
+  2024-09-30
 ]]
