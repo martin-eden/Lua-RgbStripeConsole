e4875d32e0b800c324b2d85102f3bcfe012ccce1
Martin Eden <20048064+martin-eden@users.noreply.github.com>
2024-09-30 23:05:53 +0200

    [/] [StripeWriter.SetPixels] Now emits several commands..
    .. to fit output in 64 bytes.
    
    It means we can transfer 4 pixels in 64 bytes. Sending via SetPixel()
    will require like 80 bytes.
    
    Another time savings is coming from SendLine(). I mean after-line
    delay. We're sending one line instead of four. But I've doubled
    inter-line delay. So delay is 50 ms now vs 100 ms (4 * 25) for 4 pixels.
    
    Not a big savings to be practical, but I wanted to implement it.

--- StripeWriter/SetPixels.lua
+++ StripeWriter/SetPixels.lua
@@ -24,12 +24,55 @@ return
 
     local StartIndex = GetIndex(Args.StartIndex)
     local StopIndex = GetIndex(Args.StopIndex)
+    local Length = StopIndex - StartIndex + 1
 
     assert(StartIndex <= StopIndex)
 
     local Colors = Args.Colors
     assert_table(Colors)
 
+    -- Make sure we have exact number of colors for stride
+    assert(#Colors == Length)
+
+    --[[
+      Real world. If we will send long line to Arduino, most
+      of it will be lost. To stay safe we need to send lines
+      under 64 bytes (Arduino's serial buffer size).
+
+      So one call of SetPixels() will split to several "SPR" commands.
+
+      We'll use recursion here.
+
+      Recursion depth is ((NumPixels / NumPixelsPerChunk) - 1).
+
+      Memory consumption is quadratic of recursion depth. That's
+      bad but should be acceptable for practical cases.
+    ]]
+    do
+      local NumPixelsPerChunk = 4
+      if (Length > NumPixelsPerChunk) then
+        -- Call ourselves with first chunk
+        local Head = {}
+        Head.StartIndex = StartIndex
+        Head.StopIndex = StartIndex + NumPixelsPerChunk - 1
+        Head.Colors = {}
+        table.move(Colors, 1, NumPixelsPerChunk, 1, Head.Colors)
+        self:SetPixels(Head)
+
+        -- Call ourselves with chopped remainder
+        local Tail = {}
+        Tail.StartIndex = StartIndex + NumPixelsPerChunk
+        Tail.StopIndex = StopIndex
+        Tail.Colors = {}
+        table.move(Colors, NumPixelsPerChunk + 1, Length, 1, Tail.Colors)
+        self:SetPixels(Tail)
+
+        -- We won't fall-through
+        return
+      end
+    end
+
+    -- Unpack colors and represent them as string
     local ColorsStr
     do
       local FlattenedColors = {}
@@ -37,16 +80,19 @@ return
       for Index, Color in ipairs(Colors) do
         local Red, Green, Blue = UnpackColor(Color)
 
-        table.insert(FlattenedColors, Red)
-        table.insert(FlattenedColors, Green)
-        table.insert(FlattenedColors, Blue)
+        local ColorStrFormat =
+          '%03d %03d %03d'
+        local ColorStr =
+          string.format(ColorStrFormat, Red, Green, Blue)
+
+        table.insert(FlattenedColors, ColorStr)
       end
 
-      ColorsStr = table.concat(FlattenedColors, ' ')
+      ColorsStr = table.concat(FlattenedColors, '  ')
     end
 
     local CommandFormat =
-      'SPR %d %d %s'
+      'SPR %03d %03d  %s'
 
     local Command =
       string.format(
